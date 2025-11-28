2bd8760ec881204a64756402377be951e84d62af
sri <sreelakshmi.ravipati88@gmail.com>
2025-11-28 17:29:13 +0000

    adding Ternary practiced program

---
+++ Operators/TernaryOperator.js
@@ -0,0 +1,9 @@
+/*Ternary operator  (?:)
+ =================
+ 
+    condition ? true statment : false statement */
+    
+
+let personAge=21;
+
+console.log(personAge>=18 ? " person is Allowed" : "person is Denied");
